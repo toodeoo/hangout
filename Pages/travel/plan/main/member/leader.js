00141eabe7594c55c997f2432c3a4f4e243aa80d
郑勤 <10205501439@stu.ecnu.edu.cn>
2022-07-30 15:32:46 +0800

    finish2.0

--- Pages/travel/plan/main/member/leader.js
+++ Pages/travel/plan/main/member/leader.js
@@ -5,12 +5,15 @@ Page({
      * 页面的初始数据
      */
     data: {
-        id:["洗后提"],
-        tele:["12345678910"],
-        contact:["10987654321"],
-        gender:["黄油小狗"],
-        age:["3"]
-    },
+      id:["洗后提"],
+      info:[
+        {key:"性别",value:"黄油小狗"},
+        {key:"年龄",value:"3岁"},
+        {key:"联系电话",value:"12345678910"},
+        {key:"紧急联系人",value:"12345678910"},
+        {key:"分工",value:"吃"}
+      ]
+  },
 
     ret:function(){
       wx.navigateBack({
--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 //app.js
 App({
     onLaunch: function () {
-        
+      
+      wx.login({
+        timeout: 1000,
+        success(res){
+          if(res.code){
+            wx.request({
+              url: 'https://localhost:8080/postCode',
+              data: res.code,
+              method: "POST"
+            })
+          }
+        }
+      })
+      
     },
+  
+
     globalData: {
       userInfo: null
     }
